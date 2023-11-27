47247f1789c65cd5625ad73d6781eed6d435df9b
EliottDelannee <152154324+EliottDelannee@users.noreply.github.com>
2023-11-27 11:25:22 +0100

    Update microshell.c

--- microshell.c
+++ microshell.c
@@ -18,7 +18,7 @@ while(1){
     commande[bytesRead] = '\0';
 
     if (strcmp(commande, "exit") == 0) {
-        write(STDOUT_FILENO, "\n", 2);
+        write(STDOUT_FILENO, "\n", 0);
         break;
         
         } else {
