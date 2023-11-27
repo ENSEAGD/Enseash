0a22b0d3ece0678df1d3929fc0c7f1c01cbe87a9
EliottDelannee <152154324+EliottDelannee@users.noreply.github.com>
2023-11-27 10:44:50 +0100

    Update microshell.c

--- microshell.c
+++ microshell.c
@@ -1,11 +1,31 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <strln.h>
+#string.h
 
 int main () {
 
-write(STDOUT_FILENO, "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n", 57);
+char commande[100];
+ssize_t bytesRead;
+
+while(1){
     
-    return 0;
+    write(STDOUT_FILENO, "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n", 57);
+
+    bytesRead = read(STDIN_FILENO, commande, sizeof(commande));
+    
+    commande[bytesRead] = '\0';
+
+    if (strcmp(commande, "exit") == 0) {
+        write(STDOUT_FILENO, "Au revoir !\n", 12);
+        break;
+        
+        } else {
+        
+        write(STDOUT_FILENO, "nous ne connaissons pas la commande que vous avez entrez pour l'instant je ne connais que la commande 'exit' :3 /n", 57)
+            
+        }
+    
+    
 }
 
