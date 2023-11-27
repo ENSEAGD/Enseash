1cd6b480441553c77eac615babd0d362ff3de6fa
EliottDelannee <152154324+EliottDelannee@users.noreply.github.com>
2023-11-27 11:33:24 +0100

    Update microshell.c

--- microshell.c
+++ microshell.c
@@ -1,7 +1,9 @@
 #include <unistd.h>
-#include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 #include <string.h>
+#include <stdio.h>
 
 int main () {
 
