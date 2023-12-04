12d22ed17ae8de271f2a59ffb7810aa08e7d7013
EliottDelannee <152154324+EliottDelannee@users.noreply.github.com>
2023-12-04 11:46:43 +0100

    Update microshell3.c

--- microshell3.c
+++ microshell3.c
@@ -33,12 +33,12 @@ while(1){
         if (pid == 0) {
             //Child process
             if (strlen(input)== 0) {
-                char *args[] = {"date",-1, NULL};
+                char *args[] = {"date","-1", NULL};
                 //User entered nothing so the code print the date
                 execvp(args[0], args);
             }
             else{
-                char *args[] = {input,-1, NULL};            
+                char *args[] = {input,"-1", NULL};            
                 execvp(args[0], args);
             }
             exit(EXIT_FAILURE);
