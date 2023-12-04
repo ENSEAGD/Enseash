9eee9cf85600461f6180b8431eacad06b6e0be79
EliottDelannee <152154324+EliottDelannee@users.noreply.github.com>
2023-12-04 10:39:30 +0100

    Update microshell2.c

--- microshell2.c
+++ microshell2.c
@@ -32,7 +32,7 @@ while(1){
             //Child process
             if (strlen(input) == 0) {
                 //User entered nothing so the code print the date
-                execlp("date\n", "date\n", NULL);
+                execlp("date", "date", NULL);
             }
             else if (strcmp(input, "install fortune\n") == 0) {
                 execlp("sudo", "sudo", "apt-get", "install", "fortune", NULL);
